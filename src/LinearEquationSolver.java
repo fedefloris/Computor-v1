c9bfdfaed5c5947df8f9e971f7013f11f4a96509
Federico <fzfloris@gmail.com>
2018-11-07 10:35:23 +0100

    Add 42 header

--- src/LinearEquationSolver.java
+++ src/LinearEquationSolver.java
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   LinearEquationSolver.java                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffloris <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 10:31:09 by ffloris           #+#    #+#             */
+/*   Updated: 2018/11/07 10:31:10 by ffloris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 public class LinearEquationSolver implements IEquationSolver {
 
 	public void solve(Equation equation) {
--- src/ParserException.java
+++ src/ParserException.java
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ParserException.java                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffloris <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 10:31:09 by ffloris           #+#    #+#             */
+/*   Updated: 2018/11/07 10:31:10 by ffloris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 public class ParserException extends RuntimeException {
 
   public ParserException(String message) {
