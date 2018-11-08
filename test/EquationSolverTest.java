72b8589ef5dd90cd917c218dfd6e289df67e8d1d
fedefloris <fzfloris@gmail.com>
2018-11-08 18:12:31 +0100

    Add more test cases. Update todo.txt

--- test/EquationSolverTest.java
+++ test/EquationSolverTest.java
@@ -31,16 +31,19 @@ public class EquationSolverTest {
 
 		inputs = new String[][] {
 			{"", "Reduced form: 0 = 0\nDegree: 0\nEvery value for x is a solution\n"},
+			{"0.0 = 0.0", "Reduced form: 0 = 0\nDegree: 0\nEvery value for x is a solution\n"},
+			{"00 = 0*1", "Reduced form: 0 = 0\nDegree: 0\nEvery value for x is a solution\n"},
+			{"00 = 0*0", "Reduced form: 0 = 0\nDegree: 0\nEvery value for x is a solution\n"},
 			{"1", "Reduced form: 1 * X^0 = 0\nDegree: 0\nThere is no solution because the equation is inconsistent\n"},
-			{"1 + x = 1 + x", "Reduced form: 0 = 0\nDegree: 0\nEvery value for x is a solution\n"},
+			{"1*1 + x = 1 + x", "Reduced form: 0 = 0\nDegree: 0\nEvery value for x is a solution\n"},
 			{"5 * X^2 = 5 * X^2", "Reduced form: 0 = 0\nDegree: 0\nEvery value for x is a solution\n"},
 			{"1 + x", "Reduced form: 1 * X^1 + 1 * X^0 = 0\nDegree: 1\nThe solutions is: -1.0\n"},
-			{"2 * x ^ 1 = 3", "Reduced form: 2 * X^1 - 3 * X^0 = 0\nDegree: 1\nThe solutions is: 1.5\n"},
+			{"2 * 1 * x ^ 1 = 3", "Reduced form: 2 * X^1 - 3 * X^0 = 0\nDegree: 1\nThe solutions is: 1.5\n"},
 			{"-2 * x ^ 1 = 3", "Reduced form: -2 * X^1 - 3 * X^0 = 0\nDegree: 1\nThe solutions is: -1.5\n"},
 			{"2.0 * x ^ 1 = -3.0", "Reduced form: 2 * X^1 + 3 * X^0 = 0\nDegree: 1\nThe solutions is: -1.5\n"},
-			{"5 = 4 + 7 * x", "Reduced form: -7 * X^1 + 1 * X^0 = 0\nDegree: 1\nThe solutions is: 0.14285714285714285\n"},
-			{"-1 + x + 2 = 2 - 1 + x", "Reduced form: 0 = 0\nDegree: 0\nEvery value for x is a solution\n"},
-			{"1 + x = -1 + x", "Reduced form: 2 * X^0 = 0\nDegree: 0\nThere is no solution because the equation is inconsistent\n"},
+			{"5 = 2*2 + 7 * x", "Reduced form: -7 * X^1 + 1 * X^0 = 0\nDegree: 1\nThe solutions is: 0.14285714285714285\n"},
+			{"-1 + x + 2*1 = 2 - 1 + x", "Reduced form: 0 = 0\nDegree: 0\nEvery value for x is a solution\n"},
+			{"1*1 + x = -1*1 + x", "Reduced form: 2 * X^0 = 0\nDegree: 0\nThere is no solution because the equation is inconsistent\n"},
 		};
 		testInputs(inputs);
 	}
@@ -50,8 +53,11 @@ public class EquationSolverTest {
 		String[][] inputs;
 
 		inputs = new String[][] {
-			{"x ^ 2", "Reduced form: 1 * X^2 = 0\nDegree: 2\nDiscriminant: 0.0\nThe solutions is: 0.0\n"},
-			{"5 * X^0 + 13 * X^1 + 3 * X^2 = 1 * X^0 + 1 * X^1", "Reduced form: 3 * X^2 + 12 * X^1 + 4 * X^0 = 0\nDegree: 2\nDiscriminant: 96.0\nThe two solutions are:\n-3.632993161855452\n-0.3670068381445481\n"}
+			{"x ^ 2 + 0 = 0", "Reduced form: 1 * X^2 = 0\nDegree: 2\nDiscriminant: 0.0\nThe solutions is: 0.0\n"},
+			{"5 * 10 * 0.1 * X^0 + 13 * 1.0 * X^1 + 3 * X^2 = 1 * X^0 + 1 * X^1", "Reduced form: 3 * X^2 + 12 * X^1 + 4 * X^0 = 0\nDegree: 2\nDiscriminant: 96.0\nThe two solutions are:\n-3.632993161855452\n-0.3670068381445481\n"},
+			{"x^2 - 15 * X + 56 = 0", "Reduced form: 1 * X^2 - 15 * X^1 + 56 * X^0 = 0\nDegree: 2\nDiscriminant: 1.0\nThe two solutions are:\n7.0\n8.0\n"},
+			{"+2 * X^2 + 7 * X + 4", "Reduced form: 2 * X^2 + 7 * X^1 + 4 * X^0 = 0\nDegree: 2\nDiscriminant: 17.0\nThe two solutions are:\n-2.7807764064044154\n-0.7192235935955849\n"},
+			{"5.0 * -2 * -0.5 * X^0 + 3 * X^1 + 3 * X^2 = 1 * X^0 + 0 * X^1", "Reduced form: 3 * X^2 + 3 * X^1 + 4 * X^0 = 0\nDegree: 2\nDiscriminant: -39.0\nThe two solutions are:\n-1.5408329997330663i\n0.5408329997330664i\n"}
 		};
 		testInputs(inputs);
 	}
@@ -61,7 +67,8 @@ public class EquationSolverTest {
 		String[][] inputs;
 
 		inputs = new String[][] {
-			{"x ^ 3 = -1.1", "Reduced form: 1 * X^3 + 1.1 * X^0 = 0\nDegree: 3\nThe degree is strictly greater than 2, I can't solve\n"}
+			{"x ^ 3 = -1.1", "Reduced form: 1 * X^3 + 1.1 * X^0 = 0\nDegree: 3\nThe degree is strictly greater than 2, I can't solve\n"},
+			{"x ^ 99999999", "Reduced form: 1 * X^99999999 = 0\nDegree: 99999999\nThe degree is strictly greater than 2, I can't solve\n"}
 		};
 		testInputs(inputs);
 	}
